ff9600cdbca0e272aa6cf42b1ec185b1fb2e245a
zfree <79763939@qq.com>
2019-04-10 12:57:22 +0800

    提交第一版本可以被install后使用

--- examples/main.js
+++ examples/main.js
@@ -2,15 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-router'
 import router from './router'
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import TennetcnUI from 'main/index'
 
-Vue.use(ElementUI)
 Vue.use(TennetcnUI)
-Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
