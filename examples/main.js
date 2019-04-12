687059a2d657d1a3d0dc81425e821669e2ef2965
zfree <79763939@qq.com>
2019-04-12 23:01:42 +0800

    复制了文档的相关文件

--- examples/main.js
+++ examples/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import hljs from 'highlight.js'
 import 'element-ui/lib/theme-chalk/index.css'
+import './demo-styles/index.scss'
 import './assets/styles/common.css'
 import './assets/styles/fonts/style.css'
 import TennetcnUI from 'main/index'
@@ -11,6 +13,9 @@ import ElementUI from 'element-ui'
 
 import MainFooter from './components/footer'
 import MainHeader from './components/header'
+import SideNav from './components/side-nav'
+import demoBlock from './components/demo-block'
+import FooterNav from './components/footer-nav'
 
 Vue.use(TennetcnUI)
 Vue.use(ElementUI)
@@ -18,6 +23,17 @@ Vue.config.productionTip = false
 
 Vue.component('main-footer', MainFooter)
 Vue.component('main-header', MainHeader)
+Vue.component('side-nav', SideNav)
+Vue.component('demo-block', demoBlock)
+Vue.component('footer-nav', FooterNav)
+
+router.afterEach(route => {
+  // https://github.com/highlightjs/highlight.js/issues/909#issuecomment-131686186
+  Vue.nextTick(() => {
+    const blocks = document.querySelectorAll('pre code:not(.hljs)')
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+  })
+})
 
 /* eslint-disable no-new */
 new Vue({
