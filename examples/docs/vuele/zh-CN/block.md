99b3df26689259ea472937ea7aefc5c5c1b45806
zfree <79763939@qq.com>
2019-05-04 18:23:50 +0800

    添加了block组件和示例说明

--- examples/docs/vuele/zh-CN/block.md
+++ examples/docs/vuele/zh-CN/block.md
@@ -1,3 +1,11 @@
 ## Block容器
 
 通过Block容器进行页面的分区域布局
+
+:::demo
+```html
+<tc-block title="我是一个block">
+test
+</tc-block>
+```
+:::
--- packages/block/src/block.vue
+++ packages/block/src/block.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>block</h1>
+  <div ref="blockContainer" class="block-container">
+    <div class="block-title" :style="titleStyle">
+      <slot name="title">
+        <span class="block-title-left"></span>
+        <span>{{title}}</span>
+      </slot>
+    </div>
+    <div class="block-content" :style="contentStyle">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -8,15 +16,62 @@
 export default {
   name: 'TcBlock',
   props: {
+    title: { type: String, default: '' }
   },
   data: () => ({
+    height: 100,
+    titleHeight: 30
   }),
   computed: {
+    titleStyle: function() {
+      return {
+        height: this.titleHeight + 'px',
+        lineHeight: this.titleHeight + 'px'
+      }
+    },
+    contentStyle: function() {
+      return {
+        height: (this.height - this.titleHeight) + 'px'
+      }
+    }
+  },
+  mounted() {
+    this.calcHeight()
   },
   methods: {
+    calcHeight() {
+      let height = this.$refs.blockContainer.style.height
+      this.height = parseInt(height, 10)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+.block-container{
+  border: 1px solid #cfd9e5;
+  box-shadow: 2px 2px 2px #f4f0f0;
+  .block-title{
+    padding: 5px;
+    font-size: 15px;
+    color: #333;
+    background-color: #f1f4fa;
+    padding-left: 15px;
+    border-bottom: 1px solid #cfd9e5;
+    vertical-align: middle;
+  }
+  .block-title-left{
+    width: 3px;
+    height: 15px;
+    margin-top: -2px;
+    background: #1379d2;
+    border-radius: 25%;
+    display: inline-block;
+    margin-right: 5px;
+    vertical-align: middle;
+  }
+  .block-content{
+    padding: 5px;
+  }
+}
 </style>
