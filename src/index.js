e8fc20938405f829ae4bd5926dc01b7d51c32617
zfree <79763939@qq.com>
2019-05-26 22:59:56 +0800

    发布了0.0.8版本,处理了样式

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '0.0.7',
+  version: '0.0.8',
   install,
   utilLib,
   TcBadge,
---
+++ src/styles/gulpfile.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const { series, src, dest } = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cssmin = require('gulp-cssmin')
+
+function compile() {
+  return src('./index.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      browsers: ['ie > 9', 'last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(dest('./lib'))
+}
+function compileCompoents() {
+  return src('./components/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      browsers: ['ie > 9', 'last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(dest('./lib/components'))
+}
+exports.build = series(compile, compileCompoents)
