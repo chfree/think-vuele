2b23f96aede9def71b7cbdecfaadbdd69847c023
zfree <79763939@qq.com>
2019-06-04 23:55:53 +0800

    切换了另外一个avatar组件

--- examples/docs/vuele/zh-CN/letter-avatar.md
+++ examples/docs/vuele/zh-CN/letter-avatar.md
@@ -1,42 +1,50 @@
 ## LetterAvatar
 letter avatar 头像显示
 
-源码参照[`aidewoode/vue-letter-avatar`](https://github.com/aidewoode/vue-letter-avatar)
+源码参照[`eliep/vue-avatar`](https://github.com/eliep/vue-avatar)
 
 ### 基础用法
 :::demo
 ```html
-<tc-letter-avatar name="A" /> 
-<tc-letter-avatar name="B" /> 
-<tc-letter-avatar name="free" /> 
+<tc-letter-avatar username="A" inline :rounded="false" />
+<tc-letter-avatar username="B" inline :rounded="false" />
+<tc-letter-avatar username="free" inline :rounded="false" />
 ```
 :::
 
 ### 圆形
 :::demo
 ```html
-<tc-letter-avatar name="中" :rounded=true /> 
-<tc-letter-avatar name="华" :rounded=true /> 
-<tc-letter-avatar name="人" :rounded=true /> 
-<tc-letter-avatar name="民" :rounded=true /> 
-<tc-letter-avatar name="共" :rounded=true /> 
-<tc-letter-avatar name="和" :rounded=true /> 
-<tc-letter-avatar name="国" :rounded=true /> 
+<tc-letter-avatar username="天" inline />
+<tc-letter-avatar username="地" background-color="#eee" inline />
+<tc-letter-avatar username="玄" background-color="#999" inline />
+<tc-letter-avatar username="黄" background-color="#fde" inline />
+<tc-letter-avatar username="宇" background-color="#88a" inline />
+<tc-letter-avatar username="宙" background-color="#ee8" inline />
+<tc-letter-avatar username="洪" color="#999" background-color="#369" inline />
+<tc-letter-avatar username="荒" color="#000" background-color="#abc" inline />
 ```
 :::
 
 ### 大小
 :::demo
 ```html
-<tc-letter-avatar name="大" size="70" :rounded=true /> 
-<tc-letter-avatar name="中" size="50" :rounded=true /> 
-<tc-letter-avatar name="小" size="40" :rounded=true /> 
+<tc-letter-avatar username="大" inline :size="70" />
+<tc-letter-avatar username="中" inline :size="50" />
+<tc-letter-avatar username="小" inline :size="40" />
 ```
 :::
 
 ### Attributes
 | 参数 | 说明 | 类型 | 可选值 | 默认值   |
 |------  |-----|---- |----- |---- |
-| name | avatar的名字 | String | — |  |
+| username | avatar的名字 | String | — |  |
 | size | avatar的大小 | String | — | 50 |
 | rounded | 是否显示成圆形 | Boolean | — | false |
+| inline | display采用 inline-flex 或者 flex | Boolean | - | false |
+| initial | Force the displayed initials by overriding the computed ones | String | — |  |
+| src | 使用一个图片作为avatar来显示 | String | — |  |
+| :customStyle | 自定义一个样式来覆盖原有样式 | Object | — |  |
+| backgroundColor | avatar的背景色 | String | — |  |
+| color | avatar的前景色 | String | — |  |
+| :lighten | A factor by which the background color must be lightened to produce the font color | Number | [-100,100] | 80 |
--- packages/letter-avatar/src/letter-avatar.vue
+++ packages/letter-avatar/src/letter-avatar.vue
@@ -1,49 +1,143 @@
 <template>
-<svg :style='svgStyle'>
-  <text x='50%' y='50%' text-anchor='middle' dominant-baseline='central' :style='textStyle'>
-    {{ avatarLetter }}
-  </text>
-</svg>
+  <div class="vue-avatar--wrapper" :style="[style, customStyle]" aria-hidden="true">
+    <span v-show="!this.isImage">{{ userInitial }}</span>
+  </div>
 </template>
+
 <script>
 export default {
   name: 'TcLetterAvatar',
   props: {
+    username: {
+      type: String
+    },
+    initials: {
+      type: String
+    },
+    backgroundColor: {
+      type: String
+    },
+    color: {
+      type: String
+    },
+    customStyle: {
+      type: Object
+    },
+    inline: {
+      type: Boolean
+    },
     size: {
-      type: String,
-      default: '50'
+      type: Number,
+      default: 50
+    },
+    src: {
+      type: String
     },
     rounded: {
       type: Boolean,
-      default: false
+      default: true
     },
-    name: {
-      type: String,
-      required: true
+    lighten: {
+      type: Number,
+      default: 80
+    }
+  },
+  data() {
+    return {
+      backgroundColors: [
+        '#F44336', '#FF4081', '#9C27B0', '#673AB7',
+        '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688',
+        '#4CAF50', '#8BC34A', '#CDDC39', /* '#FFEB3B' , */ '#FFC107',
+        '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B']
+    }
+  },
+  mounted() {
+    if (!this.isImage) {
+      this.$emit('avatar-initials', this.username, this.userInitial)
     }
   },
   computed: {
-    avatarLetter() {
-      return this.name[0].toUpperCase()
+    background() {
+      if (!this.isImage) {
+        return this.backgroundColor || this.randomBackgroundColor(this.username.length, this.backgroundColors)
+      }
     },
-    svgStyle() {
-      // use hsl to define the background color.
-      const letterIndex = this.avatarLetter.charCodeAt() - 64
-      const hue = (360 / 26) * letterIndex
-      const backgroundColor = `hsl(${hue}, 68%, 48%)`
-      return {
+    fontColor() {
+      if (!this.isImage) {
+        return this.color || this.lightenColor(this.background, this.lighten)
+      }
+    },
+    isImage() {
+      return Boolean(this.src)
+    },
+    style() {
+      const style = {
+        display: this.inline ? 'inline-flex' : 'flex',
         width: `${this.size}px`,
         height: `${this.size}px`,
-        borderRadius: this.rounded ? '100%' : '0',
-        background: backgroundColor
+        borderRadius: this.rounded ? '50%' : 0,
+        lineHeight: `${(this.size + Math.floor(this.size / 20))}px`,
+        fontWeight: 'bold',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
       }
+      const imgBackgroundAndFontStyle = {
+        background: `transparent url('${this.src}') no-repeat scroll 0% 0% / ${this.size}px ${this.size}px content-box border-box`
+      }
+      const initialBackgroundAndFontStyle = {
+        backgroundColor: this.background,
+        font: `${Math.floor(this.size / 2.5)}px/${this.size}px Helvetica, Arial, sans-serif`,
+        color: this.fontColor
+      }
+      const backgroundAndFontStyle = (this.isImage)
+        ? imgBackgroundAndFontStyle
+        : initialBackgroundAndFontStyle
+      Object.assign(style, backgroundAndFontStyle)
+      return style
     },
-    textStyle() {
-      return {
-        fill: 'rgba(255, 255, 255, .7)',
-        fontFamily: "'Lucida Console', Monaco, monospace",
-        fontSize: `${this.size * 0.65}px`
+    userInitial() {
+      if (!this.isImage) {
+        const initials = this.initials || this.initial(this.username)
+        return initials
       }
+      return ''
+    }
+  },
+  methods: {
+    initial(username) {
+      let parts = username.split(/[ -]/)
+      let initials = ''
+      for (var i = 0; i < parts.length; i++) {
+        initials += parts[i].charAt(0)
+      }
+      if (initials.length > 3 && initials.search(/[A-Z]/) !== -1) {
+        initials = initials.replace(/[a-z]+/g, '')
+      }
+      initials = initials.substr(0, 3).toUpperCase()
+      return initials
+    },
+    randomBackgroundColor(seed, colors) {
+      return colors[seed % (colors.length)]
+    },
+    lightenColor(hex, amt) {
+      // From https://css-tricks.com/snippets/javascript/lighten-darken-color/
+      var usePound = false
+      if (hex[0] === '#') {
+        hex = hex.slice(1)
+        usePound = true
+      }
+      var num = parseInt(hex, 16)
+      var r = (num >> 16) + amt
+      if (r > 255) r = 255
+      else if (r < 0) r = 0
+      var b = ((num >> 8) & 0x00FF) + amt
+      if (b > 255) b = 255
+      else if (b < 0) b = 0
+      var g = (num & 0x0000FF) + amt
+      if (g > 255) g = 255
+      else if (g < 0) g = 0
+      return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16)
     }
   }
 }
