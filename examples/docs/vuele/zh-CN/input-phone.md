14f115165e05f0b2bbe5189df09f22bbae5e6df2
zfree <79763939@qq.com>
2019-06-14 17:12:33 +0800

    添加了input-phone

--- examples/docs/vuele/zh-CN/input-phone.md
+++ examples/docs/vuele/zh-CN/input-phone.md
@@ -7,11 +7,62 @@
 基于该验证库的示例库[`LouisMazel/vue-phone-number-input`](https://louismazel.github.io/vue-phone-number-input/)
 
 
-### 基础示例
+### 基础示例 通用验证
 :::demo
 ```html
 {{phone}}
 <tc-input-phone v-model="phone" />
+<script>
+  export default {
+    data() {
+      return {
+        phone: ''
+      }
+    }
+  }
+</script>
+```
+:::
+
+### 基础示例 正则只验证手机号
+:::demo
+```html
+{{phone}}
+<tc-input-phone v-model="phone" valid-type="mobile" />
+<script>
+  export default {
+    data() {
+      return {
+        phone: ''
+      }
+    }
+  }
+</script>
+```
+:::
+
+### 基础示例 正则只验证座机号
+:::demo
+```html
+{{phone}}
+<tc-input-phone v-model="phone" valid-type="phone" />
+<script>
+  export default {
+    data() {
+      return {
+        phone: ''
+      }
+    }
+  }
+</script>
+```
+:::
+
+### 基础示例 正则验证座机号或座机
+:::demo
+```html
+{{phone}}
+<tc-input-phone v-model="phone" valid-type="mop" />
 <script>
   export default {
     data() {
@@ -29,3 +80,5 @@
 | 参数 | 说明 | 类型 | 可选值 | 默认值   |
 |------  |-----|---- |----- |---- |
 | country | 国家区域代码 | String | — | CN |
+| validType | 验证类型 | String | `common`,`phone`,`mobile`,`mop`,`regex` | `mop` |
+| regex | 正则表达式 | String | — | CN |
--- packages/input-phone/src/input-phone.vue
+++ packages/input-phone/src/input-phone.vue
@@ -6,10 +6,23 @@
 <script>
 import vnameMixin from 'main/mixins/vname-mixin.js'
 import { parsePhoneNumberFromString } from 'libphonenumber-js'
+/**
+ * validType: common 通用，采用libphone进行
+ *            phone 座机号
+ *            mobile 手机号
+ *            mop 手机号+座机号
+ *            regex 自定义正则
+ */
+var regexValid = {
+  mobile: /^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0-9]))\d{8}$/,
+  phone: /^(0\d{2}-?\d{8}(-\d{1,4})?)$|^(0\d{3}-?\d{7,8}(-\d{1,4})?)$/
+}
 export default {
   name: 'TcInputPhone',
   mixins: [vnameMixin],
   props: {
+    validType: { type: String, default: 'mop' },
+    regex: { type: String, default: null },
     country: { type: String, default: 'CN' },
     value: { type: Object | String | Number, default: null }
   },
@@ -19,13 +32,30 @@ export default {
   },
   methods: {
     blurHandle(e) {
-      const inPhone = e.target.value
+      var resultPhone = e.target.value
+      if (this.validType === 'common') {
+        this.libphoneValid(resultPhone)
+      } else if (this.validType === 'phone' || this.validType === 'mobile') {
+        if (!regexValid[this.validType].test(resultPhone)) {
+          resultPhone = ''
+        }
+      } else if (this.validType === 'mop') {
+        if (!regexValid.phone.test(resultPhone) && !regexValid.mobile.test(resultPhone)) {
+          resultPhone = ''
+        }
+      } else if (this.validType === 'regex') {
+        if (this.regex !== null && !this.regex.test(resultPhone)) {
+          resultPhone = ''
+        }
+      }
+      this.$emit('input', resultPhone)
+    },
+    libphoneValid(inPhone) {
       var result = this.getParsePhoneNumberFromString(inPhone, this.country)
       if (result.isValid) {
-        this.$emit('input', result.phoneNumber)
-      } else {
-        this.$emit('input', '')
+        return result.phoneNumber
       }
+      return ''
     },
     getParsePhoneNumberFromString(phoneNumber, countryCode) {
       const parsing = phoneNumber && countryCode ? parsePhoneNumberFromString(phoneNumber, countryCode) : null
